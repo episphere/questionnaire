0d5b20d3dec1a870aeeda6315e4ee1ed2c023362
Tony Petersen <tonypetersen@hotmail.com>
2023-12-01 10:52:12 -0600

    Update and rename questVersions.json to questVersions.js

--- questVersions.js
+++ questVersions.js
@@ -1,4 +1,4 @@
-{
+export default {
   "myconnect.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.0.15/replace2.js",
   "myconnect-stage.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@latest/replace2.js",
   "episphere.github.io": "https://episphere.github.io/quest/replace2.js",
