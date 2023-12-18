c9584825d9feb74c71e35d3950b2be39c2b065a0
Tony Petersen <tonypetersen@hotmail.com>
2023-12-18 10:38:32 -0600

    Update questVersions.js

--- questVersions.js
+++ questVersions.js
@@ -1,7 +1,7 @@
 const questConfig = {
   "myconnect.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.0.15/replace2.js",
   "myconnect-stage.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.0.15/replace2.js",
-  "episphere.github.io": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.0.15/replace2.js",
+  "episphere.github.io": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.0/replace2.js",
   "localhost:5000": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.0.15/replace2.js"
 }
 
