09a258e266f3b12ad05ca504bd6f999f31e5295b
Joe Armani <93854858+JoeArmani@users.noreply.github.com>
2024-06-07 09:30:07 -0400

    Update questVersions.js

--- questVersions.js
+++ questVersions.js
@@ -1,8 +1,8 @@
 const questConfig = {
   "myconnect.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.5/replace2.js",
-  "myconnect-stage.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.5/replace2.js",
+  "myconnect-stage.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.6/replace2.js",
   "episphere.github.io": "https://episphere.github.io/quest/replace2.js",
-  "localhost:5000": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.5/replace2.js"
+  "localhost:5000": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.6/replace2.js"
 }
 
 export default questConfig;
